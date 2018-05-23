a6eedb879b4442489edf89c1720944d00409dd76
xiaoyiyiyo <xiaoyiyiyo@outlook.com>
2018-05-23 22:51:51 +0800

    完善回调函数，识别select, update sql 等等

--- mybatis/src/main/java/www/xiaoyiyiyo/mybatis/proxy/AnnoMethodInvocationHandler.java
+++ mybatis/src/main/java/www/xiaoyiyiyo/mybatis/proxy/AnnoMethodInvocationHandler.java
@@ -1,13 +1,16 @@
 package www.xiaoyiyiyo.mybatis.proxy;
 
 import www.xiaoyiyiyo.common.util.C3p0Pool;
+import www.xiaoyiyiyo.mybatis.operate.Delete;
 import www.xiaoyiyiyo.mybatis.operate.Insert;
+import www.xiaoyiyiyo.mybatis.operate.Select;
+import www.xiaoyiyiyo.mybatis.operate.Update;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
+import java.lang.reflect.ParameterizedType;
 import java.sql.SQLException;
+import java.util.List;
 
 /**
  * Created by xiaoyiyiyo on 2018/5/17.
@@ -20,10 +23,37 @@ public class AnnoMethodInvocationHandler implements InvocationHandler {
 
         String sql = null;
         if (method.isAnnotationPresent(Insert.class)) {
-            //TODO checksql
+            sql = checkSql(method.getAnnotation(Insert.class).value(), "Insert");
             C3p0Pool.insert(sql, args);
+        } else if (method.isAnnotationPresent(Select.class)) {
+            sql = checkSql(method.getAnnotation(Select.class).value(), "Select");
+            Class clzz = method.getReturnType();
+            //多条记录
+            if (List.class.isAssignableFrom(clzz)) {
+                //获取list中的对象类型
+                Class<?> actualClzz =
+                        (Class<?>) ((ParameterizedType)(method.getGenericReturnType())).getActualTypeArguments()[0];
+                C3p0Pool.selectMulti(sql, args, actualClzz);
+            } else {
+                C3p0Pool.selectSingle(sql, args, clzz);
+            }
+        } else if (method.isAnnotationPresent(Update.class)) {
+            sql = checkSql(method.getAnnotation(Update.class).value(), "Update");
+            C3p0Pool.update(sql, args);
+        } else if (method.isAnnotationPresent(Delete.class)) {
+            sql = checkSql(method.getAnnotation(Delete.class).value(), "Delete");
+            C3p0Pool.delete(sql, args);
         }
         return null;
+    }
+
+    private String checkSql(String sql, String operate) throws SQLException {
+        // 获取关键字 e.g. select or update...
+        String keyOperate = sql.split(" ")[0];
+        if (null == keyOperate || !keyOperate.equalsIgnoreCase(operate)) {
+            throw new SQLException("sql语句有错误");
+        }
+        return sql;
     }
 
 }
