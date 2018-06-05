79fb1c94657a81793fe2fbe18e34ed32fff9be66
xiaoyiyiyo <xiaoyiyiyo@outlook.com>
2018-06-05 22:02:03 +0800

    添加sql其他操作，待测

--- mybatis/src/test/java/www/xiaoyiyo/mybatis/SiteMapper.java
+++ mybatis/src/test/java/www/xiaoyiyo/mybatis/SiteMapper.java
@@ -1,8 +1,10 @@
 package www.xiaoyiyo.mybatis;
 
 import www.xiaoyiyiyo.mybatis.bean.SiteDo;
+import www.xiaoyiyiyo.mybatis.operate.Delete;
 import www.xiaoyiyiyo.mybatis.operate.Insert;
 import www.xiaoyiyiyo.mybatis.operate.Select;
+import www.xiaoyiyiyo.mybatis.operate.Update;
 
 import java.util.List;
 
@@ -17,6 +19,12 @@ public interface SiteMapper {
     @Select("select * from inav_site where name = ?")
     SiteDo getSite(String name);
 
-    @Insert("insert into inav_site(site_url, ")
-    void addSite(String name);
+    @Insert("insert into inav_site(site_url, name, desc) values(?, ?, ?)")
+    void addSite(String siteUrl, String name, String desc);
+
+    @Delete("delete from inav_site where name = ?")
+    void deleteSite(String name);
+
+    @Update("update inav_site set name = ?, site_url = ?, desc = ? where name = ?")
+    void updateSite(String newName, String siteUrl, String desc, String oldName);
 }
